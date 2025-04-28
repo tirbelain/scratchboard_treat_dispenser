fdc3c70e3764c0e82d60649ed9ecdcae95047abd
Anselm Kegel <a.kegel@gentlymadstudios.com>
2025-04-28 21:39:11 +0200

    added comments, cleaned up code

--- scratchboard_treat_dispenser.ino
+++ scratchboard_treat_dispenser.ino
@@ -23,7 +23,7 @@ enum RuntimeStates
 	Dispense
 };
 
-const int16_t	CALIBRATION_PREWARM_DURATION	= 2000;		// 5 seconds - delay before noise detection calibration starts
+const int16_t	CALIBRATION_PREWARM_DURATION	= 5000;		// 5 seconds - delay before noise detection calibration starts
 
 const int8_t	PIN_SERVO						= 9;
 const int8_t	PIN_MPU_INTERRUPT				= 2;		// Define the interruption #0 pin
@@ -33,28 +33,27 @@ const uint32_t	MINUTES							= 60u * SECONDS;
 const uint32_t	HOURS							= 60u * MINUTES;
 
 
-const char *TIME_REMAINING = "Time Remaining: ";
-
-////////////////////////////////////////////////////////////
-// dispensing
-
-const uint32_t	DISPENSE_COOLDOWN				= 5000u;
-const uint8_t	DISPENSE_AMOUNT					= 2u;
-const uint32_t	DISPENSE_PERIOD_LENGTH			= 5u * MINUTES;
-const uint8_t	DISPENSE_AMOUNT_PER_PERIOD		= 3u;
-const uint32_t	DISPENSE_INTERACTION_TIME		= 3u * SECONDS;
-const uint32_t	DISPENSE_INTERACTION_TIMEOUT	= 6u * SECONDS;
-
 const int SERVO_POSITION_STACK_0				= 0;
 const int SERVO_POSITION_STACK_1				= 180;
 const int SERVO_POSITION_CENTER					= 90;
 
+////////////////////////////////////////////////////////////
+// dispensing
 
-enum DispenseStack : uint8_t
+// all these times are in milliseconds
+const uint32_t	DISPENSE_COOLDOWN				= 5000u;			// When dispensing was triggered, the next trigger will not happen until this cooldown is complete.
+const uint8_t	DISPENSE_AMOUNT					= 1u;				// The number of treats that will be dispensed when triggerd.
+const uint32_t	DISPENSE_PERIOD_LENGTH			= 60u * MINUTES;	// The period is a means of limiting the number of treats that are dispensed over
+const uint8_t	DISPENSE_AMOUNT_PER_PERIOD		= 3u;				// time. During a period only the set amount per period will be dispensed.
+const uint32_t	DISPENSE_INTERACTION_TIME		= 3u * SECONDS;		// This is a means of controlling the sensitivity for triggering. Interactions with the MPU need to be recorded for at least this amount of time before the treat is dispensed. Higher values require longer interactions.
+const uint32_t	DISPENSE_INTERACTION_TIMEOUT	= 6u * SECONDS;		// An interaction can consist of multiple small interactions, not only a continuous long one. This is the time before a interaction will time out. If another interaction is recorded within this period, this is considered the same interaction.
+
+
+enum HopperSlot : uint8_t
 {
-	stack0,
-	stack1
-} nextDispenseStack = DispenseStack::stack0;
+	slot0,
+	slot1
+} nextHopperSlot = HopperSlot::slot0;
 
 uint32_t dispenseCooldownEndTime = 0;
 
@@ -98,7 +97,7 @@ void setup()
 	analogWrite(LED_BUILTIN, 0);
 
 	servo.attach(PIN_SERVO);
-	servo.write(90); // center servo
+	servo.write(SERVO_POSITION_CENTER); // center servo
 
 	setState(RuntimeStates::Prewarm);
 }
@@ -154,14 +153,14 @@ void advanceToNextState()
 		break;
 	}
 }
-
+////////////////////////////////////////////////////////////
+// code executed when the state changes
 void onEnterState()
 {
 	switch (calibrationState)
 	{
 	case RuntimeStates::Prewarm:
 		logln("New State: Prewarm");
-		setLED(0);
 		break;
 	case RuntimeStates::PrimeRingBuffer:
 		logln("New State: PrimeRingBuffer");
@@ -172,20 +171,13 @@ void onEnterState()
 		servo.write(SERVO_POSITION_CENTER - 10);
 		delay(250);
 		servo.write(SERVO_POSITION_CENTER);
-		setLED(0);
 		break;
 	}
 }
 
 
-void setLED(uint8_t newValue)
-{
-	debugLog("\tled:");
-	debugLogln(newValue);
-	analogWrite(LED_BUILTIN, newValue);
-}
-
-
+////////////////////////////////////////////////////////////
+// sets the state machine to a new state
 void setState(RuntimeStates newState)
 {
 	calibrationState = newState;
@@ -194,6 +186,8 @@ void setState(RuntimeStates newState)
 }
 
 
+////////////////////////////////////////////////////////////
+// the first state. nothing relevant happens here. the code runs a countdown until it is completed then switches to the next state
 void prewarm()
 {
 	if (doStateCountDown(CALIBRATION_PREWARM_DURATION))
@@ -204,6 +198,11 @@ void prewarm()
 }
 
 
+////////////////////////////////////////////////////////////
+// fills the ring buffer until it is completely filled with sensor data for a couple of seconds.
+// this is done to get the average noise level for the sensors.
+// when done, the gathered min/max vales are used to calculate the deadzones where the servo
+// should not be triggered.
 void primeRingBuffer()
 {
 	if (accelSamplesX.getUsedSampleCount() == accelSamplesX.getTotalSampleCount())
@@ -230,36 +229,44 @@ void primeRingBuffer()
 }
 
 
+////////////////////////////////////////////////////////////
+// the main state for operation. this moitors accelerometer values and triggers the servo if
+// an interaction is detected.
 void monitorAccelerometerForInteraction()
 {
+	// update the ring buffer for being able to react to sensor value drift
+	// TODO: this should probably ignore large values that triggered an interaction as this will
+	// have an impact on the average value, that the detection is based upon. due to the large number
+	// of samples this shouldn't be too big of a problem though
 	accelSamplesX.add(sensorState.aaReal.x);
 	accelSamplesY.add(sensorState.aaReal.y);
 	accelSamplesZ.add(sensorState.aaReal.z);
 
-	plotSample(accelSamplesX, "accX", false, false, false, false);
-	plotSample(accelSamplesY, "accY", false, false, false, false);
-	plotSample(accelSamplesZ, "accZ", false, false, false, false);
+	//plotSample(accelSamplesX, "accX", false, false, false, false);
+	//plotSample(accelSamplesY, "accY", false, false, false, false);
+	//plotSample(accelSamplesZ, "accZ", false, false, false, false);
 
 	int16_t avgX = accelSamplesX.getAvg();
-	log("\txMin:");
-	log(avgX - deadzoneX);
-	log("\txMax:");
-	log(avgX + deadzoneX);
+	//log("\txMin:");
+	//log(avgX - deadzoneX);
+	//log("\txMax:");
+	//log(avgX + deadzoneX);
 
 	int16_t avgY = accelSamplesY.getAvg();
-	log("\tyMin:");
-	log(avgY - deadzoneY);
-	log("\tyMax:");
-	log(avgY + deadzoneY);
+	//log("\tyMin:");
+	//log(avgY - deadzoneY);
+	//log("\tyMax:");
+	//log(avgY + deadzoneY);
 
 	int16_t avgZ = accelSamplesZ.getAvg();
-	log("\tzMin:");
-	log(avgZ - deadzoneZ);
-	log("\tzMax:");
-	log(avgZ + deadzoneZ);
-	logln("");
+	//log("\tzMin:");
+	//log(avgZ - deadzoneZ);
+	//log("\tzMax:");
+	//log(avgZ + deadzoneZ);
+	//logln("");
 
 	uint32_t now = millis();
+	// check the current acceleration values against the deadzone to detect an interaction
 	if (abs(accelSamplesX.getLast() - avgX) > deadzoneX ||
 		abs(accelSamplesY.getLast() - avgY) > deadzoneY ||
 		abs(accelSamplesZ.getLast() - avgZ) > deadzoneZ)
@@ -270,12 +277,18 @@ void monitorAccelerometerForInteraction()
 		}
 	}
 
+	// check recent dispense times for values that are older than the dispense-period and remove them
+	// to unlock the next dispense
 	if (canRemoveOldestDispenseTime(now))
 	{
 		removeOldestDispenseTime();
 	}
 }
 
+
+////////////////////////////////////////////////////////////
+// performs the dispensing. after a few checks that determin whether dispensing is allowed, select
+// the hopper slot to take the treat from and trigger the servo
 void dispense(uint32_t now)
 {
 	if (hasDispenseCooldown(now))
@@ -293,7 +306,7 @@ void dispense(uint32_t now)
 	{
 		// restart interaction. there wasn't one before or the current one timed-out
 		lastInteractionStartTime = now;
-		logln("Not dispensing. Just started interaction.");
+		logln("Not dispensing. Just started the interaction.");
 		return;
 	}
 
@@ -309,15 +322,15 @@ void dispense(uint32_t now)
 	dispenseCooldownEndTime = now + DISPENSE_COOLDOWN;
 	addDispenseTime(now);
 	int servoTarget = SERVO_POSITION_CENTER;
-	switch (nextDispenseStack)
+	switch (nextHopperSlot)
 	{
-	case DispenseStack::stack0:
+	case HopperSlot::slot0:
 		servoTarget = SERVO_POSITION_STACK_0;
-		nextDispenseStack = DispenseStack::stack1;
+		nextHopperSlot = HopperSlot::slot1;
 		break;
-	case DispenseStack::stack1:
+	case HopperSlot::slot1:
 		servoTarget = SERVO_POSITION_STACK_1;
-		nextDispenseStack = DispenseStack::stack0;
+		nextHopperSlot = HopperSlot::slot0;
 		break;
 	default:
 		break;
@@ -325,30 +338,42 @@ void dispense(uint32_t now)
 
 	servo.write(servoTarget);
 	delay(250);
-	servo.write(90);
+	servo.write(SERVO_POSITION_CENTER);
 	delay(250);
 }
 
+
+////////////////////////////////////////////////////////////
+// checks whether there is a cooldown active at the moment that prevents dispensing
 bool hasDispenseCooldown(uint32_t now)
 {
 	return now < dispenseCooldownEndTime;
 }
 
+
+////////////////////////////////////////////////////////////
+// record the time for a dispense for the dispenses-per-period check
 bool addDispenseTime(uint32_t now)
 {
 	if (!hasPeriodDispensesLeft(now)) return false;
 	recentDispenseTimes[recentDispenseCount] = now;
 	++recentDispenseCount;
-	printRecentDispenseTimes();
+	DEBUG_printRecentDispenseTimes();
 	return true;
 }
 
+
+////////////////////////////////////////////////////////////
+// checks for recent dispenses that are older than the dispense-period
 bool canRemoveOldestDispenseTime(uint32_t now)
 {
 	return recentDispenseCount > 0
 		&& (recentDispenseTimes[0] + DISPENSE_PERIOD_LENGTH) < now;
 }
 
+
+////////////////////////////////////////////////////////////
+// remove old dispense times from the record to allow for more dispensing
 void removeOldestDispenseTime()
 {
 	--recentDispenseCount;
@@ -356,15 +381,42 @@ void removeOldestDispenseTime()
 	{
 		recentDispenseTimes[i] = recentDispenseTimes[i + 1];
 	}
-	printRecentDispenseTimes();
+	DEBUG_printRecentDispenseTimes();
 }
 
+
+////////////////////////////////////////////////////////////
+// checks the number of valid recent dispenses in order to determin whether dispensing is allowed at the moment
 bool hasPeriodDispensesLeft(uint32_t now)
 {
 	return recentDispenseCount < DISPENSE_AMOUNT_PER_PERIOD;
 }
 
-void printRecentDispenseTimes()
+
+////////////////////////////////////////////////////////////
+// a non-blocking countdown that displays remaining seconds
+// totalDuration is in milliseconds
+bool doStateCountDown(uint16_t totalDuration)
+{
+	static int prevCountdownValue = 0;
+
+	long timeSinceStateStart = millis() - calibrationStateChangedTime;
+	long remaining = max(0, totalDuration - timeSinceStateStart);
+	int countdownValue = ceil(remaining * 0.001f);
+	if (countdownValue != prevCountdownValue)
+	{
+		log("Time Remaining: ");
+		logln(countdownValue);
+		prevCountdownValue = countdownValue;
+	}
+
+	return countdownValue == 0;
+}
+
+
+////////////////////////////////////////////////////////////
+// debug output that logs the recent dispense times
+void DEBUG_printRecentDispenseTimes()
 {
 	log("recent dispenses: ");
 	for (int i = 0; i < recentDispenseCount; ++i)
@@ -376,25 +428,9 @@ void printRecentDispenseTimes()
 }
 
 
-bool doStateCountDown(uint16_t totalDuration)
-{
-	static int prevCountdownValue = 0;
-
-	long timeSinceStateStart = millis() - calibrationStateChangedTime;
-	long remaining = max(0, totalDuration - timeSinceStateStart);
-	int countdownValue = ceil(remaining * 0.001f);
-	if (countdownValue != prevCountdownValue)
-	{
-		log(TIME_REMAINING);
-		logln(countdownValue);
-		prevCountdownValue = countdownValue;
-	}
-
-	return countdownValue == 0;
-}
-
-/// Sends the state of the RingBuffer to the Serial Monitor/Plotter
-void plotSample(const RingBuffer &sample, const char *label, bool avg, bool min, bool max, bool offsetByAvg)
+////////////////////////////////////////////////////////////
+//  Sends the state of the RingBuffer to the Serial Monitor/Plotter
+void DEBUG_plotSample(const RingBuffer &sample, const char *label, bool avg, bool min, bool max, bool offsetByAvg)
 {
 	int16_t offset = 0;
 	if (offsetByAvg)
